7f03aad4ecb9fc36c696f782c5579ec1ada085e7
muhammadirawan <145947113+muhammadirawan@users.noreply.github.com>
2024-05-21 11:49:14 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,12 @@
+#asigment vaule
+
+x = 3
+print("nilai uas saya adalah", x)
+
+x = 6
+y = 7
+print("nilai uas saya adalah", x)
+print("nilai uas saya adalah", y)
+
+x = 1000
+print("nilai uas saya adalah", x)
