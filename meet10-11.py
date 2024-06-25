388c55d79fecc8f8d997887710214ecc3dcc392e
muhammadirawan <145947113+muhammadirawan@users.noreply.github.com>
2024-06-25 11:52:33 +0800

    Add files via upload

---
+++ meet10-11.py
@@ -0,0 +1,26 @@
+# nelajar perulangan loop
+
+#bil = 1
+#print(bil)
+#bil = bil + 2 
+#print(bil)
+#bil = bil + 2 
+
+#for kondisi
+#for (unt i; I<=10; i++)
+
+
+# perulamgan menggunakan list
+bil = [0, 1, 2, 3, 4, 5, 6]
+print(bil)
+
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
+
+# perulangan menggunakan range
+ bil2 = range(999)
+ for i in bil2 :
+     print(f"nilai i sekarang -> {i}" 
+     print("pak edi")
+
+print("nilai program 2")
